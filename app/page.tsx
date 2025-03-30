84ee59a089b1d42528a43ad446df87e134c18b32
Camillebzd <bcamille99@gmail.com>
2025-03-30 16:28:07 +0100

    [FIX] update the steps on main page

--- app/page.tsx
+++ app/page.tsx
@@ -23,16 +23,18 @@ export default function Home() {
         Ok now lets talk about the game itself. &quot;Oh no another weird NFT farming game in which I have to spend 200 eth to try it&quot;. Nah don&apos;t worry, the goal of the project is to 
         be easy and as free as it can be (you will still have to pay the gas fees, I love you but not that much).
       </p>
-      <p className={styles.paragraphe}>Start to play:</p>
-      <p>WIP...</p>
-      {/* <ol type="1" >
+      <p className={styles.pageSubtitle}>Start to play</p>
+      <ol type="1" >
         <li>Connect your wallet</li>
         <li>Go on a Etherlink testnet faucet and get some free tokens</li>
         <li>Go in the Armory section</li>
-        <li>Click on the button to create a free weapon</li>
-        <li>Go in the World page and click on monster and on the fight button to kick their ass</li>
-        <li>If you want to enhance it you will have to use XTZ tokens</li>
-      </ol> */}
+        <li>Click on the button to go and select a starter weapon to mint</li>
+        <li>Go in the Room section to create a room</li>
+        <li>Once the room is ready (weapon + deck + monster selected), you can launch the fight</li>
+      </ol>
+      <p className={styles.paragraphe}>
+        <b>Note:</b> Only one weapon and one monster are supported per fight for now. Multiplayer is coming soon.
+      </p>
     </main>
   )
 }
