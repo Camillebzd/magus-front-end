4602e1621cc650aaf1bb4f51aae18dc1fd910a17
Camillebzd <bcamille99@gmail.com>
2025-05-17 10:34:48 +0100

    [ADD] remove monsters from room system

--- app/components/MonsterListInRoom.tsx
+++ app/components/MonsterListInRoom.tsx
@@ -1,18 +1,50 @@
-import { useAppSelector } from '@/redux/hooks';
-import { Box, Text, Image, Flex } from '@chakra-ui/react';
+import { useAppDispatch, useAppSelector } from '@/redux/hooks';
+import { Box, Text, Image, Flex, CloseButton } from '@chakra-ui/react';
 import * as Monster from '@/sockets/@types/Monster';
 import { useMonstersWorld } from '@/scripts/customHooks';
+import { useState } from 'react';
+import { socketActions } from '@/redux/features/socketSlice';
 
 const MonsterListInRoom = () => {
   const room = useAppSelector((state) => state.socketReducer.room);
   const member = useAppSelector((state) => state.socketReducer.member);
+  const userId = useAppSelector((state) => state.authReducer.address);
+  const dispatch = useAppDispatch();
   const monsters = useMonstersWorld();
+  const [hoveredMonsterId, setHoveredMonsterId] = useState<string | null>(null);
 
   const entitySquare = (monsterInstance: Monster.Instance) => {
     const monster = monsters.find((monster) => monster.id === monsterInstance.id);
+    const isHovered = hoveredMonsterId === monsterInstance.uid;
 
     return (
-      <Box key={monsterInstance.uid} style={{ marginBottom: "1em" }} border={"1px solid"} borderRadius="lg" padding="1em" borderColor={"profoundgrey.200"}>
+      <Box
+        key={monsterInstance.uid}
+        style={{ marginBottom: "1em" }}
+        border={"1px solid"}
+        borderRadius="lg"
+        padding="1em"
+        borderColor={"profoundgrey.200"}
+        position={"relative"}
+        onMouseEnter={() => setHoveredMonsterId(monsterInstance.uid)}
+        onMouseLeave={() => setHoveredMonsterId(null)}
+      >
+        {isHovered && (
+          <CloseButton
+            borderRadius={"full"}
+            position="absolute"
+            top="-0.75rem"
+            right="-0.75rem"
+            size="sm"
+            bg={"red.400"}
+            _hover={{ bg: "red.500" }}
+            onClick={() => {
+              if (room.adminId === userId) {
+                dispatch(socketActions.removeMonsters([monsterInstance]));
+              }
+            }}
+          />
+        )}
         <Text>{monster?.name}</Text>
         <Text>{monsterInstance.uid.slice(0, 6) + "..." + monsterInstance.uid.slice(-4)}</Text>
         <Box style={{ marginBottom: "1em" }}>
